b0b6a8a69d9ebe910c5834da880d6e24c34e0f34
yub-hutch <142735268+yub-hutch@users.noreply.github.com>
2025-05-14 16:55:50 -0700

    Update random_deg_prediction.R

--- R/visualize_perturbation_effect.R
+++ R/visualize_perturbation_effect.R
@@ -3,6 +3,7 @@
 #' Draw scatter plot between Cohen's D and correlation in wild-type cells of KO-gene pairs.
 #'
 #' @param stat Calculated from \code{\link{calc_perturbation_effect}}.
+#' @param cor Correlation across "WT" or "All" cells.
 #' @return ggplot object.
 #'
 #' @examples
@@ -24,14 +25,51 @@
 #' plot_cohens_d_vs_cor(stat)
 #'
 #' @export
-plot_cohens_d_vs_cor <- function(stat) {
-  plot_data <- stat |> dplyr::filter(ko != gene)
-  p <- ggplot2::ggplot(plot_data, ggplot2::aes(cor_wt, abs(cd))) +
+plot_cohens_d_vs_cor <- function(stat, cor = c('WT', 'All')) {
+  cor <- match.arg(cor)
+  if (cor == 'WT') {
+    plot_data <- stat |> dplyr::filter(ko != gene) |> dplyr::mutate(cor = cor_wt)
+    xlab <- 'Correlation in WT cells'
+  } else {
+    plot_data <- stat |> dplyr::filter(ko != gene) |> dplyr::mutate(cor = cor_all)
+    xlab <- 'Correlation in all cells'
+  }
+  p <- ggplot2::ggplot(plot_data, ggplot2::aes(cor, abs(cd))) +
     ggpointdensity::geom_pointdensity() +
     ggplot2::scale_color_viridis_c(guide = "none") +
-    ggplot2::labs(x = 'Correlation in WT cells', y = "|Cohens' D|") +
+    ggplot2::labs(x = xlab, y = "|Cohens' D|") +
     ggplot2::geom_hline(yintercept = median(abs(plot_data$cd)), color = 'red', linetype = 2) +
     ggplot2::geom_vline(xintercept = 0, color = 'red', linetype = 2) +
+    ggpubr::theme_pubr()
+  return(p)
+}
+
+
+#' Plot adjusted P-values for KO gene pairs in -log10 scale
+#'
+#' @param stat Tibble containing DE statistics (See \code{\link{calc_perturbation_effect}}).
+#' @param eps Numeric, minimum value to clamp p-values to. Default is 1e-5.
+#' @return ggplot object.
+#' @export
+plot_adj_pv_for_ko_pairs <- function(stat, eps = 1e-5) {
+  stat1 <- stat |> dplyr::filter(ko > gene)
+  stat2 <- stat |> dplyr::filter(ko < gene)
+  plot_data <- stat1 |>
+    dplyr::inner_join(stat2, by = c(ko = "gene", gene = "ko"), suffix = c("_1", "_2")) |>
+    dplyr::mutate(
+      adj_pv_1_clamped = pmax(adj_pv_1, eps),
+      adj_pv_2_clamped = pmax(adj_pv_2, eps)
+    )
+  thr <- -log10(0.05)
+  p <- ggplot2::ggplot(plot_data, ggplot2::aes(-log10(adj_pv_1_clamped), -log10(adj_pv_2_clamped))) +
+    ggpointdensity::geom_pointdensity() +
+    ggplot2::scale_color_viridis_c(guide = "none") +
+    ggplot2::labs(
+      x = "-log10 adjusted p-value of gene A when gene B is perturbed",
+      y = "-log10 adjusted p-value of gene B when gene A is perturbed"
+    ) +
+    ggplot2::geom_vline(xintercept = thr, color = "red", linetype = 2) +
+    ggplot2::geom_hline(yintercept = thr, color = "red", linetype = 2) +
     ggpubr::theme_pubr()
   return(p)
 }
