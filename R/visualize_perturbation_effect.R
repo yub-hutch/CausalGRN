99b2eedbfb9378a9af699025c381e7a8100bfc04
yub-hutch <142735268+yub-hutch@users.noreply.github.com>
2025-07-09 07:41:10 -0700

    Update visualize_perturbation_effect.R

--- R/visualize_perturbation_effect.R
+++ R/visualize_perturbation_effect.R
@@ -79,3 +79,31 @@ plot_adj_pv_for_ko_pairs <- function(stat, eps = 1e-5) {
 }
 
 
+#' Plot Cohen's D for KO gene pairs
+#'
+#' @param stat Tibble containing DE statistics (See \code{\link{calc_perturbation_effect}}).
+#' @return ggplot object.
+#' @export
+plot_cd_for_ko_pairs <- function(stat) {
+  stat1 <- stat |> dplyr::filter(gene %in% ko) |> dplyr::filter(ko > gene)
+  stat2 <- stat |> dplyr::filter(gene %in% ko) |> dplyr::filter(ko < gene)
+  plot_data <- stat1 |> dplyr::inner_join(stat2, by = c(ko = "gene", gene = "ko"), suffix = c("_1", "_2"))
+  thr <- median(abs(plot_data$cd))
+  if (nrow(plot_data) > 1e4) {
+    message('Sample 10000 data points.')
+    plot_data <- plot_data |> dplyr::sample_n(size = 1e4)
+  }
+  p <- ggplot2::ggplot(plot_data, ggplot2::aes(abs(cd_1), abs(cd_2))) +
+    ggpointdensity::geom_pointdensity() +
+    ggplot2::scale_color_viridis_c(guide = "none") +
+    ggplot2::labs(
+      x = "|Cohen's D| of gene A when gene B is perturbed",
+      y = "|Cohen's D| of gene B when gene A is perturbed"
+    ) +
+    ggplot2::geom_vline(xintercept = thr, color = "red", linetype = 2) +
+    ggplot2::geom_hline(yintercept = thr, color = "red", linetype = 2) +
+    ggpubr::theme_pubr()
+  return(p)
+}
+
+
